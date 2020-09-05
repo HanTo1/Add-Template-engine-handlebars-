d8c78016f70991bcf1d911d0162f9f6875f55f4f
HanTo1 <=>
2020-09-05 10:49:45 +0700

    scss

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ const handlebars  = require('express-handlebars');
 const app = express()
 const port = 3000
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+// console.log('PATH: ', path.join(__dirname, 'public'));
 
 //HTTP logger
 app.use(morgan('combined'))
@@ -23,6 +26,10 @@ app.set("views", path.join(__dirname, "resources/views"));
 
 app.get('/', (req, res) => {
   res.render('home');
+})
+
+app.get('/new', (req, res) => {
+  res.render('new');
 })
 
 app.listen(port, () => {
